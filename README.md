33fd0134298ad6beace84c952f8d2a162e3b9269
Ivan Tang <hiimivantang@gmail.com>
2021-06-10 01:44:36 +0800

    added script to auto generate runbooks based on template

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kafka-dr-runbook-example
+
+
+2DC: 3+3 Kafka brokers; 3+3 Zookeeper nodes in Hierarchical Quorum
+
+2.5DC: 3+3 Kafka brokers; 2+2+1 Zookeeper nodes
+
+3DC: 2+2+2 Kafka brokers; 2+2+1 ZK
+
---
+++ init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+declare -a mdcType=("2DC" "2.5DC" "3DC")
+
+scenarios=( $(sed 1,1d scenarios.tsv | cut -d$'\t' -f1) )
+
+for i in "${mdcType[@]}"
+do
+    echo "Generating scenarios for $i...."
+
+    for s in "${scenarios[@]}"
+    do
+        category=$(cat scenarios.tsv | grep -e "^$s" | cut -d$'\t' -f2)
+        name=$(cat scenarios.tsv | grep -e "^$s" | cut -d$'\t' -f3)
+        description=$(cat scenarios.tsv | grep -e "^$s" | cut -d$'\t' -f4)
+
+        #echo "Category: $category"
+        #echo "Name: $name"
+        #echo "Description: $description"
+        #echo ""
+        
+        mkdir -p $i/scenarios/$s
+        FILE=$i/scenarios/$s/runbook.adoc
+        if test -f "$FILE"; then
+            echo "$FILE exists."
+        else
+            echo "Generating runbook $FILE"
+            cat __Template_.adoc | sed "s/\${scenario-id}/$s/g; s/\${name}/$name/g; s/\${category}/$category/g; s/\${description}/$description/g" > $i/scenarios/$s/runbook.adoc
+        fi
+
+    done
+done
