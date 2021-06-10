e5c0c36a7d7b3c973f024cee4a7f9277512034a9
Ivan Tang <hiimivantang@gmail.com>
2021-06-11 00:54:04 +0800

    updated runbook template

--- init.sh
+++ init.sh
@@ -13,16 +13,12 @@ do
         category=$(cat scenarios.tsv | grep -e "^$s" | cut -d$'\t' -f2)
         name=$(cat scenarios.tsv | grep -e "^$s" | cut -d$'\t' -f3)
         description=$(cat scenarios.tsv | grep -e "^$s" | cut -d$'\t' -f4)
-
-        #echo "Category: $category"
-        #echo "Name: $name"
-        #echo "Description: $description"
-        #echo ""
         
         mkdir -p $i/scenarios/$s
         FILE=$i/scenarios/$s/runbook.adoc
         if test -f "$FILE"; then
             echo "$FILE exists."
+            cat __Template_.adoc | sed "s/\${scenario-id}/$s/g; s/\${name}/$name/g; s/\${category}/$category/g; s/\${description}/$description/g" > $i/scenarios/$s/runbook.adoc
         else
             echo "Generating runbook $FILE"
             cat __Template_.adoc | sed "s/\${scenario-id}/$s/g; s/\${name}/$name/g; s/\${category}/$category/g; s/\${description}/$description/g" > $i/scenarios/$s/runbook.adoc
